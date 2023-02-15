04af1c74a787e542942ce319b6ca24c468abc462
rushi-smb <rushi@smbios.co.in>
2023-02-15 18:23:04 +0530

    Add Villa Data Store static class (Get List)

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
-﻿using MagicVilla_VillaAPI.Models;
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,11 +13,7 @@ namespace MagicVilla_VillaAPI.Controllers
         [HttpGet]
         public IEnumerable<VillaDto> GetVillas()
         {
-            return new List<VillaDto>()
-            {
-                new VillaDto{ Id = 1, Name = "Pool View" },
-                new VillaDto{ Id = 2, Name = "Beach View" }
-            };
+            return VillaStore.villaList;
         }
     }
 }
