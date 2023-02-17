82796e0d662500598775eda1cec90f73bce114b1
rushi-smb <rushi@smbios.co.in>
2023-02-17 16:15:55 +0530

    Create-Add Base Service for API (To call API)

---
+++ MagicVilla_Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,11 @@
+﻿using MagicVilla_Web.Models;
+
+namespace MagicVilla_Web.Services.IServices
+{
+    public interface IBaseService
+    {
+        // Generic Base Service
+        APIResponse responseModel { get; set; }
+        Task<T> SendAsync<T>(APIRequest apirequest);    //  Use to send API call to call our API
+    }
+}
