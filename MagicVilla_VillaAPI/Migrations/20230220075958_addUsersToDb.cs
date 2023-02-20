c77f2c4f59759c9423fa581f6ba9d2605c0010c6
rushi-smb <rushi@smbios.co.in>
2023-02-20 14:40:04 +0530

    Add Models  for Login and registration,add user repository and implement Repository

---
+++ MagicVilla_VillaAPI/Migrations/20230220075958_addUsersToDb.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVilla_VillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class addUsersToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LocalUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LocalUsers", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "ImageUrl" },
+                values: new object[] { new DateTime(2023, 2, 20, 13, 29, 57, 938, DateTimeKind.Local).AddTicks(2867), "https://dotnetmastery.com/bluevillaimages/villa3.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "ImageUrl" },
+                values: new object[] { new DateTime(2023, 2, 20, 13, 29, 57, 938, DateTimeKind.Local).AddTicks(2878), "https://dotnetmastery.com/bluevillaimages/villa1.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "ImageUrl" },
+                values: new object[] { new DateTime(2023, 2, 20, 13, 29, 57, 938, DateTimeKind.Local).AddTicks(2879), "https://dotnetmastery.com/bluevillaimages/villa4.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "CreatedDate", "ImageUrl" },
+                values: new object[] { new DateTime(2023, 2, 20, 13, 29, 57, 938, DateTimeKind.Local).AddTicks(2881), "https://dotnetmastery.com/bluevillaimages/villa5.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "CreatedDate", "ImageUrl" },
+                values: new object[] { new DateTime(2023, 2, 20, 13, 29, 57, 938, DateTimeKind.Local).AddTicks(2882), "https://dotnetmastery.com/bluevillaimages/villa2.jpg" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LocalUsers");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "ImageUrl" },
+                values: new object[] { new DateTime(2023, 2, 17, 20, 30, 49, 107, DateTimeKind.Local).AddTicks(7320), "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa3.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "ImageUrl" },
+                values: new object[] { new DateTime(2023, 2, 17, 20, 30, 49, 107, DateTimeKind.Local).AddTicks(7330), "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa1.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "ImageUrl" },
+                values: new object[] { new DateTime(2023, 2, 17, 20, 30, 49, 107, DateTimeKind.Local).AddTicks(7332), "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa4.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "CreatedDate", "ImageUrl" },
+                values: new object[] { new DateTime(2023, 2, 17, 20, 30, 49, 107, DateTimeKind.Local).AddTicks(7333), "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa5.jpg" });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "CreatedDate", "ImageUrl" },
+                values: new object[] { new DateTime(2023, 2, 17, 20, 30, 49, 107, DateTimeKind.Local).AddTicks(7335), "https://dotnetmasteryimages.blob.core.windows.net/bluevillaimages/villa2.jpg" });
+        }
+    }
+}
---
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -0,0 +1,60 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_VillaAPI.Repository.IRepository;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private string _secretKey;
+        public UserRepository(ApplicationDbContext db,IConfiguration configuration)
+        {
+            _db = db;
+            _secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+        }
+        // Check User Name is Unique or Not
+        public bool IsUniqueUser(string username)
+        {
+            var user = _db.LocalUsers.FirstOrDefault(x => x.UserName == username);
+            if (user == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // When we log in user we have to generate a token to validate that user & send that back
+        public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        {
+            // Check username & password available in table
+            var user = _db.LocalUsers.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDto.UserName.ToLower()
+            && u.Password == loginRequestDto.Password);
+            // If Not Found return Null
+            if (user == null)
+            {
+                return null;
+            }
+            // If user was found generate JWT token
+
+        }
+
+        // Add New Local User in Our Database
+        public async Task<LocalUser> Register(RegistrationRequestDto registrationRequestDto)
+        {
+            LocalUser user = new LocalUser()
+            {
+                // This is Manual Mapping we can use AutoMapper here
+                UserName = registrationRequestDto.UserName,
+                Password = registrationRequestDto.Password,
+                Name = registrationRequestDto.Name,
+                Role = registrationRequestDto.Role
+            };
+            _db.LocalUsers.Add(user); // Add USer in LocalUsers table
+            await _db.SaveChangesAsync();
+            user.Password = ""; //  Make sure before return password is empty
+            return user;
+        }
+    }
+}
